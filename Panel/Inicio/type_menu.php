5118e5d6f1201ec14f88b2e8fefdc7c388dc9924
Steven Andre Lecoña Saravia <94027479+3000Leonidas@users.noreply.github.com>
2025-01-09 18:03:16 -0400

    Update type_menu.php

--- Panel/Inicio/type_menu.php
+++ Panel/Inicio/type_menu.php
@@ -34,17 +34,3 @@
     </div>
 </body>
 </html>
-
-<!--
-Tengo que pensar mucho acerca de algo importante en el que me encuentro justo ahora y es acerca de la Nancy nos estamos distanciando mucho. Me duele y no se que hacer, no me queda mucho tiempo en este lugar y tarde o temprano.
-Me retirare y hay una gran probabilidad que jamas vuelva a este lugar y cuando llegue ese dia inevitable ya no la volvere a ver, a saber de ella y con ello. Otra persona maravillosa estaria saliendo de mi vida es un tema que estuve pensandolo y ya no podre verle sonreir en un tiempo.
-Cuando esta feliz, cuando esta ocupada hasta cuando esta enojada no estare cuando necesite a alguien para apoyarla en esos momentos complicados que uno no merece estar solo.
-Ella tiene un gran corazón pero carga mucho dentro de ello y aunque le diga que no sigua confiando que al final poco a poco la estan dañando. Me importa mucho ella y quisera hablar con ella desde el fondo de mi corazon y aunque se la respuesta -->
-<!-- No me importaria al menos decirle lo que siento que enverdad no pude olvidarla que me diera una oportunidad pero lo pienso talvez la falle.
-Lo intente varias veces pero no pude, ella es especial para mi y cuando ya no este yo no, no se en que condiciones estare por dentro aunque cueste aunque duela quiero verla.
-ver esa sonrisa que me encanto la primera vez que la vi cuadno estabamos juntos hablando solo nosotros 2 y nadie mas, verte no sabes que tan feliz soy esos momentos. 
-Aunque yo te FALLE.!... son dias en las que lamento mi toma de decision y se que no lo diria el porque eran esa decisiones en el fondo te aseguro que si o si no queria dejarte sola y ahora actualmente al final de todo de TODO!! en si dire una palabra que tu me comentaste que no te gustaria escuchar pero al final lo tendre que decir y al final a mi tampoco me gustara decirtelo y ese es un adios -->
-<!-- Al final de todo no pude mantenerme como era hay varias cosas en las que te defraude:
-1. me volvi un aguafiesta ya no soy divertido para ti.
-2. ya no soy entretenido como lo era
-3. no soy de tu edad aunque si lo tuviera talvez con los puntos anteriores de igualmanera te estaria defraudando, hay entre otros mas que son muchos que no os quiero mencionar por el echo de que estare mucho pero mucho peor conmigo otra vez y no podre calmarme -->
